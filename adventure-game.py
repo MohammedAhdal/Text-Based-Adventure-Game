885515613f1f6de8bc2fd6bde5663d41aa0c623b
malahdal1392 <144228862+malahdal1392@users.noreply.github.com>
2023-09-23 19:51:27 +0100

    week2 first commit

--- adventure-game.py
+++ adventure-game.py
@@ -1,69 +1,68 @@
-# Define the rooms and their descriptions
+
+# Creating the Game Structure
+"""
+Our game will have a simple structure with rooms and actions. 
+The player will navigate the rooms and interact with the environment by typing commands. 
+Let's start by defining the rooms and their descriptions.
+"""
 rooms = {
-    "Corridor": "You are in a corridor links to the main hall and lifts.",
-    "Main Hall": "You are in the main hall. There is a grand chandelier hanging from the ceiling, has immense L-shaped coach and a long desk. Looks like people using this area for leisure time.",
-    "Manager's Office": "You are in the manager's room. There is a desk with a comfy office chair. Look, that's the key!!",
-    "Meeting Room": "You are in the meeting room. There is a long conference table.",
-    "Team Leader's Office": "You are in the team leader's office.",
-    "Emergency Exit": "You are in the emergency room. The walls are covered in symbols leading to the emergency exit",
-    "Big Office": "You are in a big office. It is spacious and well-lit. There are several cubicles",
-    "Kitchen": "You are in the kitchen.",
-    "Balcony": "You are on the balcony. You have a view of the garden below."
+    'start': {
+        'description': '\nYou are in the reception room.',
+        'north': 'dining room'
+    },
+    'dining room': {
+        'description': '\nYou are in the dining room.',
+        'east': 'kitchen',
+        'south': 'start'
+    },
+    'kitchen': {
+        'description': '\nYou are in the kitchen.',
+        'west': 'dining room',
+        'south': 'bedroom'
+    },
+    'bedroom': {
+        'description': '\nYou are in the bedroom. You found the treasure! Congratulations!',
+        'end': True
+    }
 }
 
+# Creating a function to display the room description and available actions
+def show_room(room):
+    print(room['description'])
 
+    if 'north' in room:
+        print('There is a door to your north.')
+    if 'east' in room:
+        print('There is a door to your east.')
+    if 'south' in room:
+        print('There is a door to your south.')
+    if 'west' in room:
+        print('There is a door to your west.')
 
-# Define the room connections
-room_connections = {
-    "Main Hall": {"north": "Balcony", "south": "Corridor", "northwest": "Manager's Office", "southwest": "Team Leader's Office", "northeast": "Emergency Exit", "east": "Big Office", "southeast": "Kitchen", "west": "Team Leader's Office"},
-    "Corridor": {'north': 'Main Hall'},
-    "Manager's Office": {"east": "Main Hall", "north": "Balcony", "south": "Meeting Room"},
-    "Team Leader's Office": {"north": "Meeting Room", "east": "Main Hall"},
-    "Balcony": {'south': 'Main Hall', 'southwest': 'Manager Room'},
-    "Meeting Room": {"north": "Manager Room", "south": "Team Leader's Office", 'west': 'Main Hall'},
-    "Emergency Exit": {'east': 'Main Hall'},
-    "Big Office": {'west': 'Main Hall', 'south': 'Kitchen'},
-    "Kitchen": {'north': 'Big Office', 'east': 'Main Hall'},
-}
+# Handling User Input
+# Creating a function to handle user input and process the commands
+def get_action(room):
+    while True:
+        action = input('Where do you want to go? ').lower().strip()
 
-print("Welcome!! You are the manager of a company and you are left alone in the office because you are working overtime. Just as you were leaving, you realized you forgot your card, so you can't use the lift. You need to go to your room and get your card. Good luck!")
-print("You are in the corridor now and you need to reach your key with the options in front of you. Good luck")
-
-
-# Initialize the player's position
-current_room = "Corridor"
-
-# Main game loop
-keys_found = False
-should_continue = True
-
-while should_continue:
-    # Display the current room's description
-    print(rooms[current_room])
-
-    # List available directions
-    available_directions = room_connections[current_room].keys()
-    print('Available directions:', ', '.join(available_directions))
-
-    # Get user input
-    user_input = input('Enter your move (e.g., "north", "east", "south", "west", "southwest", "southeast", "northeast", "northwest" or "quit" to get help and finish the game): ').lower()
-
-    # Check if the user wants to quit the game
-    if user_input == 'quit':
-        print('Ok we are calling the Officer to help you. Thank you for playing')
-        should_continue = False
-    else:
-        # Check if the entered direction is valid
-        if user_input in available_directions:
-            current_room = room_connections[current_room][user_input]
-            if current_room == "Manager's Office":
-                keys_found = True
-                print("Congratulations! You found the keys. You can now leave the office.")
-                should_continue = False
+        if action == 'north' and 'north' in room:
+            return room['north']
+        elif action == 'east' and 'east' in room:
+            return room['east']
+        elif action == 'south' and 'south' in room:
+            return room['south']
+        elif action == 'west' and 'west' in room:
+            return room['west']
         else:
-            print("You can't move in that direction. Try a different direction or 'quit'.")
-    
-if keys_found:
-    print("Thank you for playing. You successfully found the keys and can now leave the office.")
-else:
-    print("Thank you for playing. Better luck next time!")
+            print('Invalid action. Try again.')
+
+# Main Game Loop
+# Creating the main game loop that will keep running until the player reaches the end
+current_room = rooms['start']
+
+while 'end' not in current_room:
+    show_room(current_room)
+    next_room = get_action(current_room)
+    current_room = rooms[next_room]
+
+print(current_room['description'])
--- mad-lib.py
+++ mad-lib.py
@@ -1,6 +1,6 @@
 def story_selector():
     while True:
-        story_select = input('Choose a number for preferred story theme [1 for funny, 2 for scary or 3 for adventure: ').lower()
+        story_select = input('Choose a number for preferred story theme [1 for funny, 2 for scary or 3 for medical: ').lower()
         if story_select in ['one', 'two', 'three']:
             int_story_select = {'one': 1, 'two': 2, 'three': 3}[story_select]
             print(f"You selected story {int_story_select}")
@@ -18,21 +18,22 @@ def story_selector():
             print('Invalid input. Please enter a number between one and three [1, 2 or 3].') 
 def madlib_story(choice):
     if choice == 1:
-        object = input('Enter an object: ')
+        verb = input('Enter a verb: ')
         name = input('Enter a name: ')
-        story = 'During my data science bootcamp, I got bored so I decided to go to my friend ' + name.capitalize() + ' so I had to leave to buy ' + object + '.'
-        print('Funny Story:')
+        object = input('Enter a small object: ')
+        story = 'During my data science bootcamp, I got bored so I decided to ' + verb + ' I saw my neighbour ' + name.capitalize() + ' driving by so I decided to throw my ' + object + '.'
+        print('Here is your funny story:')
         print(story)
     elif choice == 2:
-        object = input('Enter an object')
-        name = input('Enter a name: ')
-        story = 'I went to the GYM, I met by change my friend ' + name.capitalize() + ' so I had to leave to buy ' + object + '.'
-        print('Funny Story:')
+        beverage = input('Enter your favorite drink: ')
+        name = input('name of a singer: ')
+        story = 'I went to the GYM, by chance I saw an old friend of mine ' + name.capitalize() + ' so sat in a cafe to drink ' + beverage + '.'
+        print('Here is your scary story:')
         print(story)
     else:
-        object = input('Enter an object: ')
+        object = input('Enter a small object: ')
         name = input('Enter a name: ')
-        story = 'During my GP appointment, I met my friend ' + name.capitalize() + ' and left behind ' + object + '.'
-        print('Adventure Story:')
+        story = 'During my GP appointment, I saw Dr ' + name.capitalize() + ' it turned out that I have swallowed  ' + object + '.'
+        print('Here is your medical story:')
         print(story)
 story_selector()
